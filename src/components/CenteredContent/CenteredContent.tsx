065d7f95e87786551d5d8e05d0c81d1e69fd6186
pyoneerC <maxcomperatutti@gmail.com>
2024-07-17 11:09:08 -0300

    centered react compnent

---
+++ src/components/CenteredContent/CenteredContent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface CenteredContentProps {
+	children: React.ReactNode;
+}
+
+const CenteredContent: React.FC<CenteredContentProps> = ({ children }) => {
+	return (
+		<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+			{children}
+		</div>
+	);
+};
+
+export default CenteredContent;
