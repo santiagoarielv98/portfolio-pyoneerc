f3d0a6fc4292107c197c440e12ce46f134ca7f2c
pyoneerC <maxcomperatutti@gmail.com>
2024-07-17 13:59:26 -0300

    footer text

--- src/app/(with-footer)/blog/(posts)/layout.tsx
+++ src/app/(with-footer)/blog/(posts)/layout.tsx
@@ -9,7 +9,7 @@ export default function PostsLayout({
 		<main>
 			<article className={styles.article}>
 				{children}
-				<div className={styles.greetings}>Gracias por leer y recuerda tomar agüita 💙</div>
+				<div className={styles.greetings}>Gracias por leer 💙</div>
 			</article>
 		</main>
 	)
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.wrapper}>
-				<span className={styles.year}>Basado en el portfolio de <Link href="https://www.dresan.dev"
+				<span className={styles.year}>Inspirado en el portfolio de <Link href="https://www.dresan.dev"
 																																			target={"_blank"}>Dresan</Link>
 				</span>
 				<div className={styles.linksWrapper}>
