aa284a72f7f5094ff22a4345e256d15703a53051
pyoneerC <maxcomperatutti@gmail.com>
2024-07-19 19:48:55 -0300

    rearrange links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,22 +1,22 @@
 export const socialMediaLinks = [
 	{
-		label: "YouTube",
-		url: "https://www.youtube.com/channel/UCgZDAjllREu-3YeU-961EBg",
-	},
-	{
-		label: "itch.io",
-		url: "https://pyoneerc1.itch.io/",
+		label: "LinkedIn",
+		url: "https://www.linkedin.com/in/maximo-comperatore-74399b278",
 	},
 	{
 		label: "GitHub",
 		url: "https://github.com/pyoneerC",
 	},
 	{
-		label: "LinkedIn",
-		url: "https://www.linkedin.com/in/maximo-comperatore-74399b278",
-	},
-	{
 		label: "Email",
 		url: "mailto:maxcomperatore@gmail.com",
+	},
+	{
+		label: "YouTube",
+		url: "https://www.youtube.com/channel/UCgZDAjllREu-3YeU-961EBg",
+	},
+	{
+		label: "itch.io",
+		url: "https://pyoneerc1.itch.io/",
 	},
 ]
