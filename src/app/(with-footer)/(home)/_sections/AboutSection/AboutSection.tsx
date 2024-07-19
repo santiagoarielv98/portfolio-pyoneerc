b10650c2aabd165e091eb8ae0a2096b6ed0c0a2d
pyoneerC <maxcomperatutti@gmail.com>
2024-07-19 20:21:23 -0300

    about me

--- src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
+++ src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
@@ -9,10 +9,12 @@ export const AboutSection = () => {
           Conoce a Maximo Comperatore, especializado en desarrollo Backend con .NET y desarrollo de videojuegos.
 
           Actualmente, estudio Desarrollo de Videojuegos en la Universidad de Mendoza,
-          preparándome para un futuro profesional en tecnología.
+          preparándome para un futuro profesional en tecnología. Mis aspiraciones son trabajar en proyectos
+          útiles y desafiantes, que me permitan crecer y aprender en el campo de la tecnología.
         </p>
         <p>
-          Busco siempre ampliar mi conocimiento y contribuir a proyectos que me apasionan. Estoy comprometido con mi desarrollo profesional,
+          Busco siempre ampliar mi conocimiento y contribuir a proyectos que me apasionan.
+          Estoy comprometido con mi desarrollo profesional,
           buscando oportunidades para crecer en el campo de la tecnología y el desarrollo de software.
         </p>
       </div>
