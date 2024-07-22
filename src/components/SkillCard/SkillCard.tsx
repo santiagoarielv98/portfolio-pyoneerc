a5c7ba80f609be5ea3c3a0b50c71eb4fbe1ec584
pyoneerC <maxcomperatutti@gmail.com>
2024-07-22 10:32:00 -0300

    links on tech stack

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -1,20 +1,21 @@
-import styles from "./SkillCard.module.css"
+import styles from "./SkillCard.module.css";
 
 interface Props {
-	icon: React.ReactNode
-	brandColor: string
-	name: string
+	icon: React.ReactNode;
+	brandColor: string;
+	name: string;
+	url: string;
 }
 
-export const SkillCard: React.FC<Props> = ({ icon, brandColor, name }) => {
-	const customProperties = { "--color-brand": brandColor } as React.CSSProperties
+export const SkillCard: React.FC<Props> = ({ icon, brandColor, name, url }) => {
+	const customProperties = { "--color-brand": brandColor } as React.CSSProperties;
 
 	return (
-		<div style={customProperties} className={styles.card}>
+		<a href={url} target="_blank" rel="noopener noreferrer" className={styles.card} style={customProperties}>
 			<div className={styles.wrapper}>
 				<div className={styles.iconWrapper}>{icon}</div>
 				<span className={styles.skillName}>{name}</span>
 			</div>
-		</div>
-	)
-}
+		</a>
+	);
+};
--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -19,72 +19,86 @@ export const skills = [
 		name: "Bash",
 		icon: <BashLogo />,
 		brandColor: "#4EAA25",
+		url: "https://www.gnu.org/software/bash/",
 	},
 	{
 		name: "MongoDB",
 		icon: <MongoDbLogo />,
 		brandColor: "#47A248",
+		url: "https://www.mongodb.com/",
 	},
 	{
 		name: "PostgreSQL",
 		icon: <PostgreSqlLogo />,
 		brandColor: "#336791",
+		url: "https://www.postgresql.org/",
 	},
 	{
 		name: "MySQL",
 		icon: <MySQLLogo />,
 		brandColor: "#00758F",
+		url: "https://www.mysql.com/",
 	},
 	{
 		name: "Linux",
 		icon: <LinuxLogo />,
 		brandColor: "#444444",
+		url: "https://www.linux.org/",
 	},
 	{
 		name: "Docker",
 		icon: <DockerLogo />,
 		brandColor: "#039CC7",
+		url: "https://www.docker.com/",
 	},
 	{
 		name: "AWS",
 		icon: <AWSLogo />,
 		brandColor: "#FF9900",
+		url: "https://aws.amazon.com/",
 	},
 	{
 		name: "Python",
 		icon: <PythonLogo />,
 		brandColor: "#FFD445",
+		url: "https://www.python.org/",
 	},
 	// .NET related skills
 	{
 		name: ".NET",
 		icon: <DotNetLogo />,
 		brandColor: "#1486CA",
+		url: "https://dotnet.microsoft.com/",
 	},
 	{
 		name: "Azure",
 		icon: <AzureLogo />,
 		brandColor: "#007FFF",
+		url: "https://azure.microsoft.com/",
 	},
 	{
 		name: "C#",
 		icon: <CsharpLogo />,
 		brandColor: "#9A5196",
+		url: "https://docs.microsoft.com/en-us/dotnet/csharp/",
 	},
 	// Game development skills
 	{
 		name: "Unreal Engine",
 		icon: <UnrealLogo />,
 		brandColor: "",
+		url: "https://www.unrealengine.com/",
 	},
 	{
 		name: "Unity",
 		icon: <UnityLogo />,
 		brandColor: "",
+		url: "https://unity.com/",
 	},
 	{
 		name: "C++",
 		icon: <CppLogo />,
 		brandColor: "#659AD2",
+		url: "https://isocpp.org/",
 	},
 ];
