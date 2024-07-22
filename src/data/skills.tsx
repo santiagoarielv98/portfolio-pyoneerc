0c1698f8c1ff2228f99c629edcfb4d7103042064
pyoneerC <maxcomperatutti@gmail.com>
2024-07-22 10:13:39 -0300

    skills rearranged

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -1,24 +1,20 @@
-import { PythonLogo } from "~/components/Svg/PythonLogo"
-import { MongoDbLogo } from "~/components/Svg/MongoDbLogo"
-import { PostgreSqlLogo } from "~/components/Svg/PostgreSqlLogo"
-import { AzureLogo } from "~/components/Svg/AzureLogo"
-import { BashLogo } from "~/components/Svg/BashLogo"
-import { LinuxLogo } from "~/components/Svg/LinuxLogo"
-import { DotNetLogo } from "~/components/Svg/DotNetLogo"
-import { UnityLogo } from "~/components/Svg/UnityLogo"
-import { UnrealLogo } from "~/components/Svg/UnrealEngineLogo"
-import { CsharpLogo } from "~/components/Svg/CsharpLogo"
-import { CppLogo } from "~/components/Svg/CppLogo"
-import { DockerLogo } from "~/components/Svg/DockerLogo"
-import { MySQLLogo } from "~/components/Svg/MySQLLogo"
-import { AWSLogo } from "~/components/Svg/AWSLogo"
+import { PythonLogo } from "~/components/Svg/PythonLogo";
+import { MongoDbLogo } from "~/components/Svg/MongoDbLogo";
+import { PostgreSqlLogo } from "~/components/Svg/PostgreSqlLogo";
+import { AzureLogo } from "~/components/Svg/AzureLogo";
+import { BashLogo } from "~/components/Svg/BashLogo";
+import { LinuxLogo } from "~/components/Svg/LinuxLogo";
+import { DotNetLogo } from "~/components/Svg/DotNetLogo";
+import { UnityLogo } from "~/components/Svg/UnityLogo";
+import { UnrealLogo } from "~/components/Svg/UnrealEngineLogo";
+import { CsharpLogo } from "~/components/Svg/CsharpLogo";
+import { CppLogo } from "~/components/Svg/CppLogo";
+import { DockerLogo } from "~/components/Svg/DockerLogo";
+import { MySQLLogo } from "~/components/Svg/MySQLLogo";
+import { AWSLogo } from "~/components/Svg/AWSLogo";
 
 export const skills = [
-	{
-		name: "Linux",
-		icon: <LinuxLogo />,
-		brandColor: "#444444",
-	},
+	// Backend skills
 	{
 		name: "Bash",
 		icon: <BashLogo />,
@@ -40,39 +36,9 @@ export const skills = [
 		brandColor: "#00758F",
 	},
 	{
-		name: "Unreal Engine",
-		icon: <UnrealLogo />,
-		brandColor: "",
-	},
-	{
-		name: "C++",
-		icon: <CppLogo />,
-		brandColor: "#659AD2",
-	},
-	{
-		name: "Unity",
-		icon: <UnityLogo />,
-		brandColor: "",
-	},
-	{
-		name: ".NET",
-		icon: <DotNetLogo />,
-		brandColor: "#1486CA",
-	},
-	{
-		name: "C#",
-		icon: <CsharpLogo />,
-		brandColor: "#9A5196",
-	},
-	{
-		name: "Azure",
-		icon: <AzureLogo />,
-		brandColor: "#007FFF",
-	},
-	{
-		name: "AWS",
-		icon: <AWSLogo />,
-		brandColor: "#FF9900",
+		name: "Linux",
+		icon: <LinuxLogo />,
+		brandColor: "#444444",
 	},
 	{
 		name: "Docker",
@@ -80,8 +46,45 @@ export const skills = [
 		brandColor: "#039CC7",
 	},
 	{
+		name: "AWS",
+		icon: <AWSLogo />,
+		brandColor: "#FF9900",
+	},
+	{
 		name: "Python",
 		icon: <PythonLogo />,
 		brandColor: "#FFD445",
 	},
-]
+	// .NET related skills
+	{
+		name: ".NET",
+		icon: <DotNetLogo />,
+		brandColor: "#1486CA",
+	},
+	{
+		name: "Azure",
+		icon: <AzureLogo />,
+		brandColor: "#007FFF",
+	},
+	{
+		name: "C#",
+		icon: <CsharpLogo />,
+		brandColor: "#9A5196",
+	},
+	// Game development skills
+	{
+		name: "Unreal Engine",
+		icon: <UnrealLogo />,
+		brandColor: "",
+	},
+	{
+		name: "Unity",
+		icon: <UnityLogo />,
+		brandColor: "",
+	},
+	{
+		name: "C++",
+		icon: <CppLogo />,
+		brandColor: "#659AD2",
+	},
+];
