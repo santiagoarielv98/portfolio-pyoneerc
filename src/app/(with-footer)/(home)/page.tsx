aaafc95041123dfe8c2b49905fa8d80e612435c8
pyoneerC <maxcomperatutti@gmail.com>
2024-07-23 00:28:57 -0300

    vercel analytics

--- src/app/(with-footer)/(home)/page.tsx
+++ src/app/(with-footer)/(home)/page.tsx
@@ -4,10 +4,12 @@ import { AboutSection } from "./_sections/AboutSection"
 import { SkillsSection } from "./_sections/SkillsSection"
 import { ContactSection } from "./_sections/ContactSection"
 import ScrollToTopButton from "~/components/ScrollToTopButton/ScrollToTopButton"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
 	return (
 		<main>
+			<Analytics/>
 			<HeroSection />
 			<ScrollToTopButton />
 			<ProjectsSection />
