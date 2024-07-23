0c374c8f29dd567b300f85f5fb68e062cf66a466
pyoneerC <maxcomperatutti@gmail.com>
2024-07-23 00:09:31 -0300

    fix link

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const DRESAN_EMAIL = "maxcomperatore@gmail.com"
-export const HOST_URL = "https://dresan.dev"
+export const HOST_URL = "https://maxcomperatore.com"
 export const FORMSPREE_URL = "https://formspree.io/f"
 
 export const APP_ROUTES = [
