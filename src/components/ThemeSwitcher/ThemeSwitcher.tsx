3fb064c275f2d06dca324a939874419ec04eba03
pyoneerC <maxcomperatutti@gmail.com>
2024-07-24 12:12:21 -0300

    language switcher visuals

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,31 +1,33 @@
 "use client"
 
-import { useTheme } from "next-themes"
-import styles from "./ThemeSwitcher.module.css"
+import { useState } from "react";
+import styles from "./ThemeSwitcher.module.css";
 
 export const ThemeSwitcher = () => {
-	const { theme, setTheme } = useTheme()
-	const isLightMode = theme === "light"
-	const oppositeThemeName = isLightMode ? "oscuro" : "claro"
+	const [isSpanish, setIsSpanish] = useState(true);
 
 	const handleOnClick = () => {
-		const oppositeTheme = isLightMode ? "dark" : "light"
-		setTheme(oppositeTheme)
-	}
+		setIsSpanish(!isSpanish);
+		localStorage.setItem("language", isSpanish ? "en" : "es");
+		// Reload the page to apply the new language. prefferably done live
+
+	};
 
 	return (
 		<button
 			className={styles.button}
 			onClick={handleOnClick}
-			title={`Cambiar a ${oppositeThemeName}`}
+			title={`Cambiar a ${isSpanish ? "Inglés" : "Español"}`}
 			aria-live="polite"
 		>
 			<img
 				className={styles.flag}
 				aria-hidden="true"
-				src="\assets\icons\spain.png"
-				alt="Switch Theme"
+				src={isSpanish ? "/assets/icons/spain.png" : "/assets/icons/uk.png"}
+				alt={`Switch to ${isSpanish ? "English" : "Spanish"}`}
 			/>
 		</button>
 	)
 }
+
+export default ThemeSwitcher;
