85fe7fb6730222812ff98585970e09f37307494c
pyoneerC <maxcomperatutti@gmail.com>
2024-07-24 11:48:13 -0300

    spain flag icon language switcher

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -17,36 +17,15 @@ export const ThemeSwitcher = () => {
 		<button
 			className={styles.button}
 			onClick={handleOnClick}
-			title={`Cambiar a modo ${oppositeThemeName}`}
+			title={`Cambiar a ${oppositeThemeName}`}
 			aria-live="polite"
 		>
-			<svg
-				className={styles.sunAndMoon}
+			<img
+				className={styles.flag}
 				aria-hidden="true"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				strokeLinecap="round"
-			>
-				<circle
-					className={styles.sun}
-					cx="12"
-					cy="12"
-					r="6"
-					mask="url(#moon-mask)"
-					fill="currentColor"
-				></circle>
-				<g className={styles.sunBeams} stroke="currentColor" strokeWidth="2">
-					<line x1="12" y1="1" x2="12" y2="3"></line>
-					<line x1="12" y1="21" x2="12" y2="23"></line>
-					<line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-					<line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-					<line x1="1" y1="12" x2="3" y2="12"></line>
-					<line x1="21" y1="12" x2="23" y2="12"></line>
-					<line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-					<line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-				</g>
-			</svg>
+				src="\assets\icons\spain.png"
+				alt="Switch Theme"
+			/>
 		</button>
 	)
 }
