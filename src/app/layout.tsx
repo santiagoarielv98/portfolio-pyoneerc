5ea9eaf524db22031a5be749940231c2367d5de6
Santiago Ariel <santiagoarielv98@gmail.com>
2024-07-25 12:35:28 -0300

    chore: Remove unused code for fetching locale in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { SvgMasks } from "~/components/Svg/SvgMasks"
 import { Header } from "~/components/Header"
 
 import {NextIntlClientProvider} from 'next-intl';
-import {getLocale, getMessages} from 'next-intl/server';
+import {getMessages} from 'next-intl/server';
 
 import "~/styles/reset.css"
 import "~/styles/globals.css"
@@ -26,8 +26,6 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode
 }>) {
-	const locale = await getLocale();
-
 	const messages = await getMessages();
 
 	return (
