def2d9bb153473913c3bf03e209ecd2747ae7b73
pyoneerC <maxcomperatutti@gmail.com>
2024-07-28 21:26:41 -0300

    other projects section

--- src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
+++ src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
@@ -5,7 +5,7 @@ import styles from "./ProjectsSection.module.css"
 export const ProjectsSection = () => {
 	return (
 		<section className="section-wrapper">
-			<h2 className={styles.title}>Últimos proyectos</h2>
+			<h2 className={styles.title}>Proyectos Destacados</h2>
 			<div className={styles.projectsWrapper}>
 				{coverProjects.map((project) => (
 					<ProjectCard key={project.name} {...project} />
--- src/app/(with-footer)/Otherprojects/page.tsx
+++ src/app/(with-footer)/Otherprojects/page.tsx
@@ -4,6 +4,8 @@ import { PostCard } from "~/components/PostCard";
 import styles from "./page.module.css";
 import ScrollToTopButton from "~/components/ScrollToTopButton/ScrollToTopButton";
 import Link from "next/link";
+import { coverProjects } from "~/data/projects";
+import { ProjectCard } from "~/components/ProjectCard";
 
 export const metadata = {
 	title: "Max Comperatore - Blog",
@@ -15,32 +17,13 @@ export default async function Blog() {
 	const sortedPosts = sortPostsByDate({ posts });
 
 	return (
-		<main className={styles.wrapper}>
-			<h1 className={styles.title}>Otros proyectos (EN CONSTRUCCION)</h1>
-			<Link href="/" className={styles.goHomeLink}>
-				← Volver a la página principal
-			</Link>
-			<p className={styles.note}>
-				Note: Esta sección aún está en desarrollo. Pronto podrás acceder directamente desde el botón al final de los proyectos principales sin necesidad de pasar por el menú principal.
-			</p>
-			<ul className={styles.projectList}>
-				<li>Rendimientos Mercado Pago</li>
-				<li>Unreal Engine Project Cleaner</li>
-				<li>The Great Chocolate Caper</li>
-				<li>Hangman</li>
-				<li>Array Utils</li>
-				<li>The Dungeon</li>
-				<li>Pixelated Drift</li>
-				<li>FOSS (Free Open Source Software) - en desarrollo</li>
-			</ul>
-			<div className={styles.projectFeatures}>
-				<p>Añade tags a los proyectos para filtrarlos por categorías y usa el botón para explorar todos los proyectos con un tag específico.</p>
-				<p>Esta sección es sin ánimo de lucro.</p>
+		<section className="section-wrapper">
+			<h2 className={styles.title}>Otros proyectos</h2>
+			<div className={styles.projectsWrapper}>
+				{coverProjects.map((project) => (
+					<ProjectCard key={project.name} {...project} />
+				))}
 			</div>
-			<div className={styles.performance}>
-				Este portfolio está optimizado para cualquier dispositivo con un diseño minimalista, limpio y elegante, utilizando TailwindCSS. Puntuaciones de PageSpeed: 98 en velocidad de página y 100 en SEO. Responsivo a todos los dispositivos y a light mode tambien.
-			</div>
-			<ScrollToTopButton />
-		</main>
+		</section>
 	);
 }
