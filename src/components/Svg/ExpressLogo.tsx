923adb42a3fd703e71a12991d9b6a56d1bb3f203
pyoneerC <maxcomperatutti@gmail.com>
2024-07-30 19:21:38 -0300

    express logo fix

--- src/components/Svg/ExpressLogo.tsx
+++ src/components/Svg/ExpressLogo.tsx
@@ -5,7 +5,7 @@ export const ExpressLogo = (props: SVGProps<SVGSVGElement>) => (
 		xmlns="http://www.w3.org/2000/svg"
 		width={55}
 		height={55}
-		viewBox="-4 -4 32 32"
+		viewBox="-2 -2 28 28"
 		fill="none"
 		{...props}
 	>
