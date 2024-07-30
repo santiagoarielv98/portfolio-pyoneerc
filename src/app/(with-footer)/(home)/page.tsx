77a72fd46a47c1aa44db2625d85b6fcd6b54ec4f
pyoneerC <maxcomperatutti@gmail.com>
2024-07-30 19:06:54 -0300

    social previews

--- src/app/(with-footer)/(home)/page.tsx
+++ src/app/(with-footer)/(home)/page.tsx
@@ -5,29 +5,10 @@ import { SkillsSection } from "./_sections/SkillsSection"
 import { ContactSection } from "./_sections/ContactSection"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import Head from "next/head"
 
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>Max Comperatore - Desarrollador Backend y de Videojuegos</title>
-				<meta name="description" content="Max Comperatore's personal portfolio showcasing projects and skills in backend and game development." />
-
-				{/* Open Graph / Facebook */}
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://maxcomperatore.com/" />
-				<meta property="og:title" content="Max Comperatore - Desarrollador Backend y de Videojuegos" />
-				<meta property="og:description" content="Max Comperatore's personal portfolio showcasing projects and skills in backend and game development." />
-				<meta property="og:image" content="https://maxcomperatore.com/assets/images/projects/otherprojectscover/portfolio.webp" />
-
-				{/* Twitter */}
-				<meta property="twitter:card" content="summary_large_image" />
-				<meta property="twitter:url" content="https://maxcomperatore.com/" />
-				<meta property="twitter:title" content="Max Comperatore - Desarrollador Backend y de Videojuegos" />
-				<meta property="twitter:description" content="Max Comperatore's personal portfolio showcasing projects and skills in backend and game development." />
-				<meta property="twitter:image" content="https://maxcomperatore.com/assets/images/projects/otherprojectscover/portfolio.webp" />
-			</Head>
 			<main>
 				<Analytics />
 				<SpeedInsights />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,13 @@ import { Header } from "~/components/Header"
 import "~/styles/reset.css"
 import "~/styles/globals.css"
 import "~/styles/utils.css"
-
 export const metadata = {
 	metadataBase: new URL("https://maxcomperatore.com"),
-	title: "Max Comperatore - Backend & Game Developer",
-	description: "Explora el portafolio de Max Comperatore, un desarrollador pragmático enfocado en backend y desarrollo de videojuegos.",
+	title: "Max Comperatore - Desarrollador Backend y de Videojuegos",
+	description: "Un desarrollador pragmático enfocado en backend y desarrollo de videojuegos.",
 	authors: [{ name: "Max Comperatore" }],
 	creator: "Max Comperatore",
 }
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="es" suppressHydrationWarning>
@@ -21,19 +19,37 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<meta charSet="UTF-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<title>{metadata.title}</title>
-			<meta name="description" content={metadata.description} />
-			<meta name="author" content={metadata.authors[0].name} />
-			<meta name="creator" content={metadata.creator} />
-
+			<meta name="description"
+						content="Portafolio personal de Max Comperatore que muestra proyectos y habilidades en desarrollo backend y de videojuegos." />
+			<meta name="author" content="Max Comperatore" />
+			<meta name="creator" content="Max Comperatore" />
+			<meta property="og:image"
+						content="https://maxcomperatore.com/assets/images/projects/otherprojectscover/portfolio.webp" />
+			{/* Open Graph / Facebook */}
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content="https://maxcomperatore.com/" />
+			<meta property="og:title" content="Max Comperatore - Desarrollador Backend y de Videojuegos" />
+			<meta property="og:description"
+						content="Portafolio personal de Max Comperatore que muestra proyectos y habilidades en desarrollo backend y de videojuegos." />
+			<meta property="og:image"
+						content="https://maxcomperatore.com/assets/images/projects/otherprojectscover/portfolio.webp" />
+			{/* Twitter */}
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:url" content="https://maxcomperatore.com/" />
+			<meta property="twitter:title" content="Max Comperatore - Desarrollador Backend y de Videojuegos" />
+			<meta property="twitter:description"
+						content="Max Comperatore's personal portfolio showcasing projects and skills in backend and game development." />
+			<meta property="twitter:image"
+						content="https://maxcomperatore.com/assets/images/projects/otherprojectscover/portfolio.webp" />
 			<script async src="https://www.googletagmanager.com/gtag/js?id=G-N5ZZD243ZP"></script>
 			<script
 				dangerouslySetInnerHTML={{
 					__html: `
-							window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
-							gtag('js', new Date());
-							gtag('config', 'G-N5ZZD243ZP');
-						`,
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-N5ZZD243ZP');
+      `,
 				}}
 			/>
 		</head>
