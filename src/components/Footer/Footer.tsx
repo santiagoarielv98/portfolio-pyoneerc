6e41789056585ad8af927ce7e853f2647e217b65
pyoneerC <maxcomperatutti@gmail.com>
2024-08-01 13:35:21 -0300

    better copyright icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,8 +9,14 @@ export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.wrapper}>
-        <span className={styles.year}>
-          &copy; {currentYear} - Maximo Comperatore
+				<span className={styles.year}>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+							 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+							 className="icon icon-tabler icons-tabler-outline icon-tabler-copyright"><path stroke="none"
+																																														 d="M0 0h24v24H0z"
+																																														 fill="none" /><path
+						d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path
+						d="M14 9.75a3.016 3.016 0 0 0 -4.163 .173a2.993 2.993 0 0 0 0 4.154a3.016 3.016 0 0 0 4.163 .173" /></svg>{currentYear} - Maximo Comperatore
         </span>
 				<div className={styles.linksWrapper}>
 					{socialMediaLinks.map(({ url, label, icon, className }) => (
