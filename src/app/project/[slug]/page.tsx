1daaf4e18abb602fd4690fb54e21f8bc841c2013
pyoneerC <maxcomperatutti@gmail.com>
2024-08-01 11:42:35 -0300

    prev and next button icons on featured projects

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -96,6 +96,12 @@ export default function Project({ params }: Props) {
 					href={`/project/${prevProjectSlug}`}
 				>
 					Anterior
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+							 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+							 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left">
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M15 6l-6 6l6 6" />
+					</svg>
 				</NextLink>
 				<NextLink
 					data-text="Siguiente"
@@ -103,6 +109,12 @@ export default function Project({ params }: Props) {
 					href={`/project/${nextProjectSlug}`}
 				>
 					Siguiente
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+							 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+							 className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right">
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M9 6l6 6l-6 6" />
+					</svg>
 				</NextLink>
 			</nav>
 		</>
