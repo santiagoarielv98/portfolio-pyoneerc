16bec9795b4e60932940110e60549a0fb498d7ff
pyoneerC <maxcomperatutti@gmail.com>
2024-08-01 20:15:23 -0300

    new footer icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,11 @@ import { socialMediaLinks } from "~/data/links"
 import { Link } from "~/components/Ui/Link"
 import styles from "./Footer.module.css"
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import { LinkedInIcon } from "~/components/Svg/LinkedInIcon"
+import { GithubIcon } from "~/components/Svg/GithubIcon"
+import { EmailIcon } from "~/components/Svg/EmailIcon"
+import { YoutubeIcon } from "~/components/Svg/YoutubeIcon"
+import { ItchioIcon } from "~/components/Svg/ItchioIcon"
 
 export const Footer = () => {
 	const currentYear = new Date().getFullYear()
@@ -22,6 +27,11 @@ export const Footer = () => {
 					{socialMediaLinks.map(({ url, label, icon, className }) => (
 						<Link key={url} className={`${styles.link} ${styles[className]}`} href={url} target="_blank">
 							<i className={icon} aria-hidden="true"></i> {label}
+							<LinkedInIcon />
+							<GithubIcon />
+							<EmailIcon />
+							<YoutubeIcon />
+							<ItchioIcon />
 						</Link>
 					))}
 				</div>
