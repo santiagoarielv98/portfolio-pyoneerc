708d7e4a87d54188f66fabb4e5ebfaceaf67a679
pyoneerC <maxcomperatutti@gmail.com>
2024-08-01 10:32:10 -0300

    fix logo to outline in blog

--- src/app/(with-footer)/blog/(posts)/layout.tsx
+++ src/app/(with-footer)/blog/(posts)/layout.tsx
@@ -13,7 +13,13 @@ export default function PostsLayout({
 				{children}
 				<div className={styles.greetings}>Gracias por tu tiempo. Nos vemos en el próximo post!</div>
 				<Link className={styles.goHomeLink} href="/blog">
-					<i className="fas fa-book-open-reader"></i>
+					<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48">
+						<g fill="none" stroke="currentColor" strokeWidth="4">
+							<path strokeLinecap="round" strokeLinejoin="round"
+										d="M24 21v23c-3.291-4-13.371-4-18-4V18c9.874 0 16.114 2 18 3m0 0v23c3.291-4 13.371-4 18-4V18c-9.874 0-16.114 2-18 3" />
+							<circle cx="24" cy="12" r="8" />
+						</g>
+					</svg>
 					Volver al Blog
 				</Link>
 			</article>
--- src/components/ArticleHeader/ArticleHeader.tsx
+++ src/components/ArticleHeader/ArticleHeader.tsx
@@ -14,7 +14,13 @@ export const ArticleHeader: React.FC<Props> = ({ title, date }) => {
 	return (
 		<div className={styles.wrapper}>
 			<Link className={styles.goHomeLink} href="/blog">
-				<i className="fas fa-book-open-reader"></i>
+				<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48">
+					<g fill="none" stroke="currentColor" strokeWidth="4">
+						<path strokeLinecap="round" strokeLinejoin="round"
+									d="M24 21v23c-3.291-4-13.371-4-18-4V18c9.874 0 16.114 2 18 3m0 0v23c3.291-4 13.371-4 18-4V18c-9.874 0-16.114 2-18 3" />
+						<circle cx="24" cy="12" r="8" />
+					</g>
+				</svg>
 				Volver al Blog
 			</Link>
 			<h1 className={styles.title}>{title}</h1>
