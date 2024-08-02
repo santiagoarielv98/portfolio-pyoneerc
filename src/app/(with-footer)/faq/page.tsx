be478bfa4ee46947597e51dd8c53296128083fb9
pyoneerC <maxcomperatutti@gmail.com>
2024-08-02 12:56:17 -0300

    smiley

--- src/app/(with-footer)/faq/page.tsx
+++ src/app/(with-footer)/faq/page.tsx
@@ -475,9 +475,10 @@ const FAQ = () => {
   <span className={styles.paragraph} style={{ color: "lime" }}>
   ¡Muchas gracias por leer hasta el final!
   </span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-               className="icon icon-tabler icons-tabler-outline icon-tabler-mood-smile-beam">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+               className="icon icon-tabler icons-tabler-outline icon-tabler-mood-smile-beam"
+               style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: '0px' }}>
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M12 21a9 9 0 1 1 0 -18a9 9 0 0 1 0 18z" />
             <path d="M10 10c-.5 -1 -2.5 -1 -3 0" />
