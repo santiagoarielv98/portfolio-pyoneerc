d11808d425c6abdf5aa18f0d0a9f3adfd6d2627e
pyoneerC <maxcomperatutti@gmail.com>
2024-08-02 13:05:37 -0300

    fix amrgin right clock

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -44,7 +44,7 @@ const Clock = ({ className }) => {
 
 	return (
 		<span className={`${styles.clock} ${className}`}>
-			<svg style={{marginRight: '8px', fontSize: '15px'}} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+			<svg style={{marginRight: '5px', fontSize: '15px'}} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
 					 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
 					 className="icon icon-tabler icons-tabler-outline icon-tabler-clock"><path stroke="none" d="M0 0h24v24H0z"
 																																										 fill="none" /><path
