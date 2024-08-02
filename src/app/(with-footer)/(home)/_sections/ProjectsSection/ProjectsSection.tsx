998cd25b856d19113647c6978fd961ad17383f3a
pyoneerC <maxcomperatutti@gmail.com>
2024-08-01 23:48:36 -0300

    see more projects icon fix

--- src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
+++ src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
@@ -12,7 +12,8 @@ export const ProjectsSection = () => {
 				))}
 				<div className={"styles.button-wrapper"}>
 					<a href="/Otherprojects" className={styles.link}>
-						Ver más proyectos
+						Ver más iniciativas
+						<div className={styles.icon}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
 								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
 								 className="icon icon-tabler icons-tabler-outline icon-tabler-code-plus">
@@ -22,6 +23,7 @@ export const ProjectsSection = () => {
 							<path d="M6 19a2 2 0 0 1 -2 -2v-4l-1 -1l1 -1v-4a2 2 0 0 1 2 -2" />
 							<path d="M18 19a2 2 0 0 0 2 -2v-4l1 -1l-1 -1v-4a2 2 0 0 0 -2 -2" />
 						</svg>
+						</div>
 					</a>
 				</div>
 			</div>
