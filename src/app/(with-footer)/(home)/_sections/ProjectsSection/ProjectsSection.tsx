1b0951fe416704baa20a301de067ba7b83d1fd33
pyoneerC <maxcomperatutti@gmail.com>
2024-08-01 23:48:56 -0300

    fix ixon attributes

--- src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
+++ src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
@@ -15,7 +15,7 @@ export const ProjectsSection = () => {
 						Ver más iniciativas
 						<div className={styles.icon}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
 								 className="icon icon-tabler icons-tabler-outline icon-tabler-code-plus">
 							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 							<path d="M9 12h6" />
