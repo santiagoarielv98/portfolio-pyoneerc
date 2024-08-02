3488d80a682eee8d5fafa8771422d83bbad065b7
pyoneerC <maxcomperatutti@gmail.com>
2024-08-02 12:47:19 -0300

    clock icon on hero section

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -44,7 +44,11 @@ const Clock = ({ className }) => {
 
 	return (
 		<span className={`${styles.clock} ${className}`}>
-			<i className="fa-regular fa-clock" style={{ marginRight: '8px', fontSize: '15px' }} />
+			<svg style={{marginRight: '8px', fontSize: '15px'}} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+					 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+					 className="icon icon-tabler icons-tabler-outline icon-tabler-clock"><path stroke="none" d="M0 0h24v24H0z"
+																																										 fill="none" /><path
+				d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M12 7v5l3 3" /></svg>
 			{getLocalTime(time)} (GMT-3)
         </span>
 	);
