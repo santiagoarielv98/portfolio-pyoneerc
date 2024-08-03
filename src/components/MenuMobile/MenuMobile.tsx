7bfefb3a89f0e11759ee22da8a27cca7daa38fc4
pyoneerC <maxcomperatutti@gmail.com>
2024-08-03 11:56:38 -0300

    better menu mobile

--- src/components/MenuMobile/MenuMobile.tsx
+++ src/components/MenuMobile/MenuMobile.tsx
@@ -63,10 +63,50 @@ export const MenuMobile = () => {
 					/>
 					<div className={styles.block}/>
 					<div className={styles.themeWrapper2}>
-						<span>Idioma </span> <LanguageSwitcher />
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-braces">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2" />
+						</svg>
+						<span>Idioma </span>
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-protocol">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M5 14v.015" />
+							<path d="M5 10.015v.015" />
+						</svg>
+						<LanguageSwitcher />
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-braces">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M17 4a2 2 0 0 1 2 2v3a2 3 0 0 0 2 3a2 3 0 0 0 -2 3v3a2 2 0 0 1 -2 2" />
+						</svg>
 					</div>
 					<div className={styles.themeWrapper}>
-						<span>Tema </span> <ThemeSwitcher />
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-braces">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2" />
+						</svg>
+						<span>Tema </span>
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-protocol">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M5 14v.015" />
+							<path d="M5 10.015v.015" />
+						</svg>
+						<ThemeSwitcher />
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+								 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+								 className="icon icon-tabler icons-tabler-outline icon-tabler-braces">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M17 4a2 2 0 0 1 2 2v3a2 3 0 0 0 2 3a2 3 0 0 0 -2 3v3a2 2 0 0 1 -2 2" />
+						</svg>
 					</div>
 				</div>
 			</div>
