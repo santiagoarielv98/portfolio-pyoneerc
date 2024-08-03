da84b34357d0e711667eb5377a1ea4ee550fd459
pyoneerC <maxcomperatutti@gmail.com>
2024-08-03 10:20:16 -0300

    Iniciativas.tolower()

--- src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
+++ src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
@@ -11,7 +11,7 @@ export const ProjectsSection = () => {
 					<ProjectCard key={project.name} {...project} />
 				))}
 				<div className={"styles.button-wrapper"}>
-					<a href="/Iniciativas" className={styles.link}>
+					<a href="/iniciativas" className={styles.link}>
 						Ver más iniciativas
 						<div className={styles.icon}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
