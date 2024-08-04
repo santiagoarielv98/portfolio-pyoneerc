d41584c5db31137fe37308b438cf6683f65a935d
pyoneerC <maxcomperatutti@gmail.com>
2024-08-03 22:43:49 -0300

    better home icons

--- src/app/(with-footer)/blog/page.tsx
+++ src/app/(with-footer)/blog/page.tsx
@@ -22,13 +22,8 @@ export default async function Blog() {
 				Artículos sobre desarrollo de software y videojuegos.
 			</p>
 			<Link className={styles.goHomeLink} href="/">
-				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-						 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-						 className="icon icon-tabler icons-tabler-outline icon-tabler-home">
-					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-					<path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-					<path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-					<path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+				<svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24">
+					<path fill="currentColor" d="M6 19h3v-6h6v6h3v-9l-6-4.5L6 10zm-2 2V9l8-6l8 6v12h-7v-6h-2v6zm8-8.75" />
 				</svg>
 				Volver a la página principal
 			</Link>
