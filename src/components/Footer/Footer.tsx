1ceab6a75d6cb99d91e9160853edc41c9d185bd5
pyoneerC <maxcomperatutti@gmail.com>
2024-08-05 15:22:48 -0300

    better copy icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -23,13 +23,8 @@ export const Footer = () => {
 		<footer className={styles.footer}>
 			<div className={styles.wrapper}>
         <span className={styles.year}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-							 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-							 className="icon icon-tabler icons-tabler-outline icon-tabler-copyright">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-            <path d="M14 9.75a3.016 3.016 0 0 0 -4.163 .173a2.993 2.993 0 0 0 0 4.154a3.016 3.016 0 0 0 4.163 .173" />
-          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor"
+																																																	 d="M10 16h4q.425 0 .713-.288T15 15v-2h-2v1h-2v-4h2v1h2V9q0-.425-.288-.712T14 8h-4q-.425 0-.712.288T9 9v6q0 .425.288.713T10 16m2 6q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" /></svg>
 					{currentYear} - Maximo Comperatore
         </span>
 				<div className={styles.linksWrapper}>
