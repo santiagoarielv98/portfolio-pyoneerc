1a83e8ac8ab3a39457019d1dfe7ec9e2cead9d28
pyoneerC <maxcomperatutti@gmail.com>
2024-08-05 10:16:09 -0300

    / translated to en

--- src/app/(with-footer)/(home)/_sections/ContactSection/ContactSection.tsx
+++ src/app/(with-footer)/(home)/_sections/ContactSection/ContactSection.tsx
@@ -1,12 +1,16 @@
 import clsx from "clsx"
 import { CopyEmailButton } from "~/components/CopyEmailButton"
 import styles from "./ContactSection.module.css"
+import { useTranslations } from "next-intl"
 
 export const ContactSection = () => {
+	const t = useTranslations("ContactSection")
+
 	return (
 		<section className={clsx("section-wrapper", styles.section)}>
 			<h2 className={styles.title}>
-				<span>¿Quieres iniciar un proyecto?</span>
+				<span>{t("question")}</span>
+				
 			</h2>
 			<CopyEmailButton />
 		</section>
--- src/app/(with-footer)/(home)/_sections/SkillsSection/SkillsSection.tsx
+++ src/app/(with-footer)/(home)/_sections/SkillsSection/SkillsSection.tsx
@@ -1,11 +1,15 @@
 import { skills } from "~/data/skills"
 import { SkillCard } from "~/components/SkillCard"
 import styles from "./SkillsSection.module.css"
+import { useTranslations } from "next-intl"
 
 export const SkillsSection = () => {
+	const t = useTranslations("SkillsSection")
 	return (
 		<section className="section-wrapper">
-			<h2 className={styles.title}>Tecnologías empleadas recientemente</h2>
+			<h2 className={styles.title}>
+				{t("title")}
+			</h2>
 			<div className={styles.skillWrapper}>
 				{skills.map((skill) => (
 					<SkillCard key={skill.name} {...skill} />
