88061ca1dea534dd8b932cf47562ca1b205202c2
pyoneerC <maxcomperatutti@gmail.com>
2024-08-05 10:00:34 -0300

    project section translated

--- src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
+++ src/app/(with-footer)/(home)/_sections/ProjectsSection/ProjectsSection.tsx
@@ -1,18 +1,22 @@
 import { coverProjects } from "~/data/projects";
 import { ProjectCard } from "~/components/ProjectCard";
 import styles from "./ProjectsSection.module.css";
+import { useTranslations } from "next-intl";
 
 export const ProjectsSection = () => {
+	const t = useTranslations("ProjectsSection");
 	return (
 		<section className="section-wrapper">
-			<h2 className={styles.title}>Proyectos Destacados</h2>
+			<h2 className={styles.title}>
+				{t("title")}
+			</h2>
 			<div className={styles.projectsWrapper}>
 				{coverProjects.map((project) => (
 					<ProjectCard key={project.name} {...project} />
 				))}
 				<div className={"styles.button-wrapper"}>
 					<a href="/iniciativas" className={styles.link}>
-						Ver más iniciativas
+						{t("seemore")}
 						<div className={styles.icon}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
 								 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
