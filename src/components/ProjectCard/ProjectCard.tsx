d541f8eb2762bcca94ddba70cac90910301e8577
pyoneerC <maxcomperatutti@gmail.com>
2024-08-06 12:11:58 -0300

    cards animations in mobile

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,14 +1,41 @@
-import Link from 'next/link'
-import type { CoverProject } from '~/types'
-import styles from './ProjectCard.module.css'
+"use client"
+
+import React, { useRef, useEffect, useState } from 'react';
+import Link from 'next/link';
+import type { CoverProject } from '~/types';
+import styles from './ProjectCard.module.css';
 
 interface Props extends CoverProject {}
 
 export const ProjectCard: React.FC<Props> = ({ slug, imageUrl, name, description }) => {
-	const href = `/project/${slug}`
+	const href = `/project/${slug}`;
+	const cardRef = useRef(null);
+	const [isVisible, setIsVisible] = useState(false);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					setIsVisible(true);
+					observer.unobserve(entry.target);
+				}
+			});
+		}, { threshold: 0 });
+
+		if (cardRef.current) {
+			observer.observe(cardRef.current);
+		}
+
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
 
 	return (
-		<article className={styles.card}>
+		<article
+			ref={cardRef}
+			className={`${styles.card} ${isVisible ? styles.cardVisible : styles.cardInitial}`}
+		>
 			<Link href={href} legacyBehavior>
 				<a className={styles.link}>{name}</a>
 			</Link>
@@ -25,5 +52,5 @@ export const ProjectCard: React.FC<Props> = ({ slug, imageUrl, name, description
 			<h3 className={styles.title}>{name}</h3>
 			<p>{description}</p>
 		</article>
-	)
+	);
 }
